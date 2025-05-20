d133c913459fdad97e5db5c92b054d13a2bfe548
calvuzs3 <calvetti.luca@gmail.com>
2025-05-20 09:31:05 +0200

    Revision of May 20 2025

--- zsh/dot-config/zshrc.d/aliases.zsh
+++ zsh/dot-config/zshrc.d/aliases.zsh
@@ -1,19 +1,22 @@
-# Aliases
+# Application aliases
+alias n="nvim"
+
+# Since trash is installed 
+alias rm='echo "This is not the command you want to use"; return false; '
+
+# Since EZA is installed 
+if [[ $(which eza) ]] ; then 
+  # echo 'EZA found'
+  alias ls="eza --color=always --icons=always"
+else 
+   alias "ls --color=always" 
+fi
+
+# Other aliases
+alias grep='grep --color=auto'
 alias ll="ls -l"
 alias la="ls -a"
 alias lt="ls -T"
 alias lla="ls -l -a"
 
-# Application aliases
-alias n="nvim"
-alias rm='echo "This is not the command you want to use"; return false; '
-alias ls="eza --color=always --icons=always"
-
-
-# Since trash is installed 
-if [[ $(which eza) ]] ; then 
-  # echo 'EZA found'
-else 
-   alias "ls ..color=always" 
-fi
 
